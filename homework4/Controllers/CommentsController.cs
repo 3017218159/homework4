c209af670d3d0cdf323da69ce14746a9e639457c
lichen <1010043642@qq.com>
2019-05-08 23:13:40 +0800

    添加项目文件。

---
+++ homework4/Controllers/CommentsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using homework4.Models;
+
+namespace homework4.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private static List<Comment> comments = new List<Comment>()
+        {
+            new Comment{ID=1, Author="User1", Text="Hello1"},
+            new Comment{ID=2, Author="User2", Text="Hello2"},
+            new Comment{ID=3, Author="User3", Text="Hello3"}
+        };
+
+        private static List<int> ids = new List<int>
+        {
+            1,2,3
+        };
+
+        public IEnumerable<Comment> Get()
+        {
+            return comments;
+        }
+
+        public Comment Get(int id)
+        {
+            var comment = comments.FirstOrDefault((p) => p.ID == id);
+            if (comment == null)
+            {
+                return new Comment { ID = 0, Author = "Null", Text = "False ID" };
+            }
+            else
+            {
+                int index = ids.IndexOf(id);
+                return comments[index];
+            }
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ homework4/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace homework4.Models
+{
+    public class Comment
+    {
+        public Comment() { }
+
+        public Comment(string text, string author)
+        {
+            Text = text;
+            Author = author;
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Author is too long! This was validated on the server.")]
+        public string Author { get; set; }
+
+    }
+
+}
